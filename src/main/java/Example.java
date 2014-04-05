6eb46f6067cb378297bdb42680f8907e39cbbf27
Edward KIM <fharenheit@gmail.com>
2014-04-05 21:26:25 +0900

    Initial commit

---
+++ src/main/java/Example.java
@@ -0,0 +1,20 @@
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class Example {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Example.class, args);
+	}
+
+}
