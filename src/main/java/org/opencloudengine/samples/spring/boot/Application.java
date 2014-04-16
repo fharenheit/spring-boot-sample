888ee4c5a5533158babfa4631d3e829de730d43f
Edward KIM <fharenheit@gmail.com>
2014-04-17 01:27:18 +0900

    Quartz 추가.

--- src/main/java/org/opencloudengine/samples/spring/boot/Application.java
+++ src/main/java/org/opencloudengine/samples/spring/boot/Application.java
@@ -21,7 +21,16 @@ public class Application {
 	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws Exception {
-		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		System.out.println("  _____ _                 _                     ____        _       _       _                           _               \n" +
+			" |  ___| | __ _ _ __ ___ (_)_ __   __ _  ___   | __ )  __ _| |_ ___| |__   | |    __ _ _   _ _ __   ___| |__   ___ _ __ \n" +
+			" | |_  | |/ _` | '_ ` _ \\| | '_ \\ / _` |/ _ \\  |  _ \\ / _` | __/ __| '_ \\  | |   / _` | | | | '_ \\ / __| '_ \\ / _ \\ '__|\n" +
+			" |  _| | | (_| | | | | | | | | | | (_| | (_) | | |_) | (_| | || (__| | | | | |__| (_| | |_| | | | | (__| | | |  __/ |   \n" +
+			" |_|   |_|\\__,_|_| |_| |_|_|_| |_|\\__, |\\___/  |____/ \\__,_|\\__\\___|_| |_| |_____\\__,_|\\__,_|_| |_|\\___|_| |_|\\___|_|   \n" +
+			"                                  |___/                                                                                 \n");
+
+		SpringApplication app = new SpringApplication(Application.class);
+		app.setShowBanner(false);
+		ApplicationContext ctx = app.run(args);
 
 		ProductService service = ctx.getBean(ProductService.class);
 		Product product = new Product();
