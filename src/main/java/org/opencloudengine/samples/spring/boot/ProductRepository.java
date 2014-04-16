5d044f4a3d6a3453bdef5f4bcd6a8908cd213eff
Edward KIM <fharenheit@gmail.com>
2014-04-17 01:07:45 +0900

    Quartz 추가.

--- src/main/java/org/opencloudengine/samples/spring/boot/ProductRepository.java
+++ src/main/java/org/opencloudengine/samples/spring/boot/ProductRepository.java
@@ -1,17 +1,29 @@
 package org.opencloudengine.samples.spring.boot;
 
-import org.springframework.stereotype.Component;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-@Component("prodDAO")
-public class ProductRepository extends BaseRepository {
+@Repository("prodDAO")
+public class ProductRepository {
+
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
 
 	public Product getProduct(int id) {
 		System.out.println("In DAO class");
 		return null;
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED)
 	public Product insert(Product product) {
-		this.getSession().persist(product);
+		this.sessionFactory.getCurrentSession().persist(product);
 		return product;
 	}
 }
