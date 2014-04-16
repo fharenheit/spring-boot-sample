90b6a0d3eea2914bcbf11f929ec7595f024c4059
Edward KIM <fharenheit@gmail.com>
2014-04-17 00:28:59 +0900

    Quartz 추가.

--- src/main/java/org/opencloudengine/samples/spring/boot/Application.java
+++ src/main/java/org/opencloudengine/samples/spring/boot/Application.java
@@ -2,11 +2,9 @@ package org.opencloudengine.samples.spring.boot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationPreparedEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -17,27 +15,19 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource(value = {"classpath:/applicationContext.xml"})
 public class Application {
 
-	@Autowired
-	ProductService service;
-
 	/**
 	 * SLF4J Logging
 	 */
 	private static Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication application = new SpringApplication();
-		application.getListeners().add(new ApplicationListener<ApplicationPreparedEvent>() {
-			@Override
-			public void onApplicationEvent(ApplicationPreparedEvent event) {
-				ProductService service = event.getApplicationContext().getBean(ProductService.class);
-				Product product = new Product();
-				product.setName("Hello World");
-				service.addProduct(product);
-				logger.info("{}", product);
-			}
-		});
-		application.run(Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+		ProductService service = ctx.getBean(ProductService.class);
+		Product product = new Product();
+		product.setName("Hello World");
+		service.addProduct(product);
+		logger.info("{}", product);
 	}
 
 }
